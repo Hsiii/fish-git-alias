c186f17d07297a87d0965952a8539e9ed7480b70
陳曦 <eriksonchen@gmail.com>
2026-01-19 20:09:00 +0800

    add config

---
+++ config.fish
@@ -0,0 +1,41 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    # Custom Greeting
+    function fish_greeting
+        # Silencing the greeting cleanly
+    end
+
+    # git abbreviations
+    # Automatically detects default branch (main/master)
+    abbr -a gm 'git switch (git symbolic-ref refs/remotes/origin/HEAD | sed "s@^refs/remotes/origin/@@") && git pull'
+    abbr -a gw 'git switch'
+    abbr -a gwc 'git switch -c'
+    abbr -a gwz 'git switch -'
+    abbr -a gr 'git fetch && git rebase (git symbolic-ref refs/remotes/origin/HEAD | sed "s@^refs/remotes/origin/@@")'
+    abbr -a gf 'git push --force-with-lease'
+    abbr -a gz 'git reset --soft HEAD~1'
+    abbr -a gb 'git for-each-ref --sort=-committerdate --format="%(refname:short) | %(committerdate:relative) | %(authorname)" refs/heads'
+    abbr -a gd 'git branch -d' # Safe delete
+    abbr -a gD 'git branch -D' # Force delete
+    abbr -a gc "git fetch -p && git for-each-ref --format '%(refname:short) %(upstream:track)' refs/heads | awk '\$2 == \"[gone]\" {print \$1}' | xargs -I % git branch -d %"
+    abbr -a gst 'git status'
+    
+    # Utility abbreviations
+    abbr -a cf '$EDITOR ~/.config/fish/config.fish'
+end
+
+# PATH management
+# fish_add_path is smarter than set -gx PATH
+fish_add_path /opt/homebrew/bin
+fish_add_path /usr/local/bin
+fish_add_path ~/.local/bin
+fish_add_path /Users/hsi/.antigravity/antigravity/bin
+
+# Node Path (Note: Consider using a fish-nvm plugin for better version management)
+fish_add_path /Users/hsi/.nvm/versions/node/v20.17.0/bin
+
+# Ensure EDITOR is set for 'cf' abbreviation
+if not set -q EDITOR
+    set -gx EDITOR vim
+end
